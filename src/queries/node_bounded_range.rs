0713473d425096b60c5d7b9b5dcdd20b67c54649
Boshen <boshenc@gmail.com>
2024-10-30 22:15:24 +0800

    feat: export `Version`

--- src/queries/node_bounded_range.rs
+++ src/queries/node_bounded_range.rs
@@ -1,14 +1,14 @@
 use std::cmp::Ordering;
 
 use super::{Distrib, QueryResult};
-use crate::{data::node::NODE_VERSIONS, semver::loose_compare};
+use crate::data::node::NODE_VERSIONS;
 
 pub(super) fn node_bounded_range(from: &str, to: &str) -> QueryResult {
     let distribs = NODE_VERSIONS
         .iter()
         .filter(|version| {
-            matches!(loose_compare(version, from), Ordering::Greater | Ordering::Equal)
-                && matches!(loose_compare(version, to), Ordering::Less | Ordering::Equal)
+            matches!(version.loose_compare(from), Ordering::Greater | Ordering::Equal)
+                && matches!(version.loose_compare(to), Ordering::Less | Ordering::Equal)
         })
         .map(|version| Distrib::new("node", version.to_string()))
         .collect();
