29c9c66aaa16dbdd2f0aea6e1d54fd8d3db691aa
Boshen <1430279+Boshen@users.noreply.github.com>
2025-05-29 01:25:28 +0000

    Update browserslist

--- src/generated/electron_to_chromium.rs
+++ src/generated/electron_to_chromium.rs
@@ -186,6 +186,7 @@ pub static ELECTRON_VERSIONS: &[(ElectronVersion, &str)] = &[
     (ElectronVersion::new(35u16, 2u16), "134"),
     (ElectronVersion::new(35u16, 3u16), "134"),
     (ElectronVersion::new(35u16, 4u16), "134"),
+    (ElectronVersion::new(35u16, 5u16), "134"),
     (ElectronVersion::new(36u16, 0u16), "136"),
     (ElectronVersion::new(36u16, 1u16), "136"),
     (ElectronVersion::new(36u16, 2u16), "136"),
