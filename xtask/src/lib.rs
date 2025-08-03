90820612402c2d55802aaaed601ef6efffa9e471
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2025-08-04 02:47:03 +0900

    refactor: use binary_search for region matching
    example binary size (inspect.exe): 952,832B -> 949,248B

--- xtask/src/lib.rs
+++ xtask/src/lib.rs
@@ -32,16 +32,6 @@ fn generate_file(file: &str, token_stream: proc_macro2::TokenStream) {
     fs::write(root().join("src/generated").join(file), code).unwrap();
 }
 
-fn create_ranges(v: &Vec<Vec<u8>>) -> Vec<(u32, u32)> {
-    let mut offset = 0;
-    let mut ranges = vec![];
-    for values in v {
-        ranges.push((offset as u32, (offset + values.len()) as u32));
-        offset += values.len();
-    }
-    ranges
-}
-
 fn create_range_vec(v: &Vec<Vec<u8>>) -> Vec<u32> {
     let mut offset = 0;
     // [start0, start1, ..., startN, endN]
