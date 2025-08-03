c0dd6ca0ef3b91f88dcf0a91f6ade9712781d433
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2025-08-04 03:16:37 +0900

    refactor: use deflate instead of gzip
    example binary size (inspect.exe): 949,248B -> 917,504B

--- src/data/caniuse/compression.rs
+++ src/data/caniuse/compression.rs
@@ -1,11 +1,11 @@
 use std::io::Read;
 
 use bincode::BorrowDecode;
-use flate2::read::GzDecoder;
+use flate2::read::DeflateDecoder;
 
 /// Decompress gzip-compressed data
-pub fn decompress_gzip(compressed_data: &[u8]) -> Vec<u8> {
-    let mut decoder = GzDecoder::new(compressed_data);
+pub fn decompress_deflate(compressed_data: &[u8]) -> Vec<u8> {
+    let mut decoder = DeflateDecoder::new(compressed_data);
     let mut decompressed = Vec::new();
     decoder.read_to_end(&mut decompressed).expect("Failed to decompress data");
     decompressed
