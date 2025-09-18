512f027dd9d74f407e825d1c5b7623f16f9ca50f
Boshen <boshenc@gmail.com>
2025-09-18 12:18:56 +0800

    feat: compress node_versions data to reduce binary size (#295)

--- src/queries/last_n_node_major.rs
+++ src/queries/last_n_node_major.rs
@@ -2,11 +2,11 @@ use super::{Distrib, QueryResult};
 use crate::data::node::NODE_VERSIONS;
 
 pub(super) fn last_n_node_major(count: usize) -> QueryResult {
-    let mut vec = NODE_VERSIONS.iter().rev().map(|version| version.major()).collect::<Vec<_>>();
+    let mut vec = NODE_VERSIONS().iter().rev().map(|version| version.major()).collect::<Vec<_>>();
     vec.dedup();
     let minimum = vec.into_iter().nth(count - 1).unwrap_or_default();
 
-    let distribs = NODE_VERSIONS
+    let distribs = NODE_VERSIONS()
         .iter()
         .filter(|version| version.major() >= minimum)
         .rev()
